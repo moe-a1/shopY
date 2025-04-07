76b1e51d87b22439347c3fb22803200be0016e46
Ahmed <ahmed22012501@gmail.com>
2025-04-07 16:12:44 +0200

    buttons-commingSoon-LandingPage Update

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, NavigationEnd, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
-  imports: [ RouterModule ],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  isBazaarPage = false;
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isBazaarPage = event.urlAfterRedirects === '/bazaar';
+      });
+  }
 }
