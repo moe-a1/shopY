0598ca9a834f7e5908e5b78e2e443cda3cace337
Ahmed <ahmed22012501@gmail.com>
2025-04-22 13:37:08 +0200

    Removed Some console.logs
    Removed Some console.logs

--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -41,7 +41,6 @@ export class ProductsComponent {
     ).subscribe(
       (data: any) => {
         this.products = data;
-        console.log('Fetched products:', this.products); // Check if the seller is populated
       },
       (error) => {
         console.error('Error fetching products:', error);
